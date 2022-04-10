53bdd1942a39e6ec6dfa8a7b969eb286a70afe31
Sneh Patel <snehp491@gmail.com>
2022-04-10 14:24:33 -0400

    search stock api and get results plus price results

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 let cryptoUrl = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd"
 
-const coinbaseBaseUrl = 'https://www.coinbase.com/api/v2/assets/search?base=USD&country=US&filter=all&include_prices=true&limit=5&order=asc&page=1&resolution=day&sort=rank';
+const coinbaseBaseUrl = 'https://www.coinbase.com/api/v2/assets/search?base=USD&country=US&filter=all&include_prices=true&limit=3&order=asc&page=1&resolution=day&sort=rank';
 
 fetch(cryptoUrl).then(function(cryptoResponse){
     return cryptoResponse.json()
@@ -34,6 +34,7 @@ $.ajax(settings).done(function (response) {
     }
 });
 
+
 // Fetch crypto results and return a promise after extracting the json response
 function searchCrypto(input) {
     let cryptoUrl = coinbaseBaseUrl + "&query=" + input;
@@ -43,8 +44,29 @@ function searchCrypto(input) {
     });
 }
 
+// Fetch stock results and return a promise after extracting the json response
+function searchStocks(input) {
+
+    let stockUrl = 'https://yh-finance.p.rapidapi.com/auto-complete?region=US&q=' + input;
+
+    return fetch(stockUrl, {
+        method: "GET",
+        headers: {
+            "X-RapidAPI-Host": "yh-finance.p.rapidapi.com",
+            "X-RapidAPI-Key": "85c2606b4emsh1b3962545e80b24p1c20dejsnc207af3b768f"
+        }
+    }).then((stockResponse) => {
+        return stockResponse.json()
+    });
+}
+
 function search($event) {
+    
     console.log('input: ' + $event.target.value);
+
+    if ($event.target.value.length < 3) {
+        return;
+    }
 
     const results = new Array();
     searchCrypto($event.target.value)
@@ -67,8 +89,45 @@ function search($event) {
 
         return results;
     })
-    .then((results) => {
-        console.log(results);
+    .then((existingResults) => {
+        searchStocks($event.target.value)
+        .then((stockResults) => {
+            console.log(stockResults);
+            console.log(existingResults);
+            for (i = 0; i < 3 && i < stockResults['quotes'].length; i++) {
+                const item = stockResults['quotes'][i];
+                const row = {
+                    name: item.longname,
+                    ticker: item.symbol
+                };
+
+                const settings = {
+                    "async": true,
+                    "crossDomain": true,
+                    "url": "https://yh-finance.p.rapidapi.com/stock/v2/get-summary?region=US&symbol=" + item.symbol,
+                    "method": "GET",
+                    "headers": {
+                        "X-RapidAPI-Host": "yh-finance.p.rapidapi.com",
+                        "X-RapidAPI-Key": "85c2606b4emsh1b3962545e80b24p1c20dejsnc207af3b768f"
+                    }
+                };
+                
+                $.ajax(settings).done(function (response) {
+                    console.log(response);
+
+                    row['marketCap'] = response['price']['marketCap']['raw'];
+                    row['percentChange'] = parseFloat((response['price']['regularMarketChangePercent']['raw'] * 100).toFixed(2)),
+                    row['price'] = response['price']['regularMarketPrice']['raw'];
+                    row['recentPrices'] = null;
+                    
+                });
+
+                results.push(row);
+            }
+        });
+    }).then((finalResults) => {
+        console.log('\n\n');
+        console.log(finalResults);
     });
 }
 
